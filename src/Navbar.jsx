30eba4c20fce10943a1e3c0a4418251d9a94ade5
Tutsus <nevertrollxd@gmail.com>
2023-08-28 13:53:52 +0700

    modified:   package-lock.json

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,7 +1,16 @@
+import { FaShoppingBasket } from 'react-icons/fa';
+
 export default function Navbar() {
     return(
         <>
-            <h1>Hello</h1>
+            <h1>ELYSIAN STORE</h1>
+            <ul>
+                <li>HOME</li>
+                <li>ABOUT</li>
+                <li>SHOP</li>
+                <li>CONTACT</li>
+                <li><FaShoppingBasket /></li>
+            </ul>
         </>
     )
 }
