87239a654eb0aeee6a1be02625775160399382f3
Tutsus <nevertrollxd@gmail.com>
2023-08-29 00:52:19 +0700

    modified:   src/App.css

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,3 +1,4 @@
+import './Header.css'
 export default function Header() {
     return (
         <nav className="Header-Main">
--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,12 +1,22 @@
 import { FaShoppingBasket } from 'react-icons/fa';
 import { LuAlignJustify } from "react-icons/lu";
+import { useState } from 'react';
+import './Navbar.css'
 import Logo from "./assets/Logo-04.png";
 
+
+
 export default function Navbar() {
+    const [navbarToggle, setNavbarToggle] = useState(true);
+
+    function handleNavbar() {
+        setNavbarToggle(!navbarToggle);
+    }
+
     return(
         <>
             <nav className='Navbar-Overall'>
-            <a href="#"><img className='Navbar-Logo' src={Logo} alt="Logo" /></a>
+                <a href="#"><img className='Navbar-Logo' src={Logo} alt="Logo" /></a>
                 <ul className='Navbar-List'>
                     <li className='Navbar-Item'><a href="#">HOME</a></li>
                     <li className='Navbar-Item'><a href="#">SHOP</a></li>
@@ -16,7 +26,16 @@ export default function Navbar() {
                 </ul>
                 <ul className='Navbar-Menu'>
                     <li className='Navbar-Item'><a href="#">< FaShoppingBasket size={32} color='#FAF1E4' className='Navbar-Icon'/></a></li>
-                    <li className='Navbar-Item'><a href="#">< LuAlignJustify size={32} color='#FAF1E4' className='Navbar-Icon'/></a></li>
+                    <li onClick={handleNavbar} className='Navbar-Item'><a href="#">< LuAlignJustify size={32} color='#FAF1E4' className='Navbar-Icon'/></a></li>
+                </ul>
+            </nav>
+            <nav className={navbarToggle ? 'Side-Open' : 'Side-Close'}>
+                <ul className='Side-List'>
+                    <li className='Side-Item'>HOME</li>
+                    <li className='Side-Item'>SHOP</li>
+                    <li className='Side-Item'>ABOUT</li>
+                    <li className='Side-Item'>CONTACT</li>
+                    <li className='Side-Item'>FEEDBACK</li>
                 </ul>
             </nav>
         </>
