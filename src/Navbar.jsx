a17b5ce84f04eb5ca9bf496ca906db1cd8be0ebd
Tutsus <nevertrollxd@gmail.com>
2023-08-28 14:03:16 +0700

    modified:   src/Navbar.jsx

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,16 +1,22 @@
 import { FaShoppingBasket } from 'react-icons/fa';
+import { FaTshirt } from 'react-icons/faT';
 
 export default function Navbar() {
     return(
         <>
-            <h1>ELYSIAN STORE</h1>
-            <ul>
-                <li>HOME</li>
-                <li>ABOUT</li>
-                <li>SHOP</li>
-                <li>CONTACT</li>
-                <li><FaShoppingBasket /></li>
-            </ul>
+            <nav className='Navbar'>
+                <div className='Navbar-Logo'>
+                    <h1><FaTshirt /></h1>
+                    <h1>ELYSIAN STORE</h1>
+                </div>
+                <ul className='Navbar-List'>
+                    <li className='Navbar-Item'>HOME</li>
+                    <li className='Navbar-Item'>ABOUT</li>
+                    <li className='Navbar-Item'>SHOP</li>
+                    <li className='Navbar-Item'>CONTACT</li>
+                    <li className='Navbar-Item'><FaShoppingBasket /></li>
+                </ul>
+            </nav>
         </>
     )
 }
