581c805989f9d1eefdc719ca5305a6ef1344a6e5
Tutsus <nevertrollxd@gmail.com>
2023-09-06 00:32:32 +0700

    modified:   src/Header.jsx

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,4 +1,5 @@
 import './Header.css';
+import Logo from "./assets/Logo-01.png";
 
 export default function Header() {
     const slogan = '"T-shirts speak louder than words"';
@@ -6,7 +7,7 @@ export default function Header() {
         <nav className='Header-Main'>
             <div className='Header-Text'>
                 <p className='Header-Welcome'>Welcome to</p>
-                <img className='Header-Logo' src="src/assets/Logo-01.png" alt="Logo" />
+                <img className='Header-Logo' src={Logo} alt="Logo" />
                 <p className='Header-Slogan'>{slogan}</p>
                 <div className='Header-Button'>
                     <a href="#"><button className='Header-Shop' type="button" style={{cursor: "pointer"}}>SHOP NOW!!!</button></a>
