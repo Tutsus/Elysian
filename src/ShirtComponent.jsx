80fff7ec84ccb563dc46e3ae49a42e6e467903ec
Tutsus <nevertrollxd@gmail.com>
2023-09-08 08:42:37 +0700

    modified:   src/ShirtComponent.jsx

--- src/ShirtComponent.jsx
+++ src/ShirtComponent.jsx
@@ -1,7 +1,8 @@
+import KillorDie from "./assets/Black - Kill or Die-03.png";
+import WhiteSkull from "./assets/Black - White Skull-03.png";
+import PubStreet from "./assets/Black - Pub Street-03.png";
+
 export default function ShirtComponent() {
-    const KillorDie = "src/assets/Black - Kill or Die-03.png";
-    const WhiteSkull = "src/assets/Black - White Skull-03.png";
-    const DarkGreenPlantnet = "src/assets/Cream - Dark Green Plantnet-03.png";
 
     return (
     <>
@@ -12,8 +13,8 @@ export default function ShirtComponent() {
             <button className="Component-Shirt-Button" type="button">Add to cart</button>
         </div>
         <div className="Component-Shirt-Parent">
-            <img className="Component-Shirt-Img" src={DarkGreenPlantnet} alt="Kill or Die" />
-            <p className="Component-Shirt-Name">T-Shirt | Green Plantnet</p>
+            <img className="Component-Shirt-Img" src={PubStreet} alt="Pub Street" />
+            <p className="Component-Shirt-Name">T-Shirt | Pub Street</p>
             <p className="Component-Shirt-Price">$8.99</p>
             <button className="Component-Shirt-Button" type="button">Add to cart</button>
         </div>
