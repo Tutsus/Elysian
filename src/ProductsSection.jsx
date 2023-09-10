a685b0c75cf7b5c5a0806fcbd714ac0bd54b0e9a
Tutsus <nevertrollxd@gmail.com>
2023-09-10 13:21:41 +0700

    modified:   src/App.css

--- src/ProductsSection.jsx
+++ src/ProductsSection.jsx
@@ -1,10 +1,15 @@
 import './App.css'
+import { useState } from 'react';
 import ShirtComponent from './ShirtComponent'
 import KillorDie from "./assets/Black - Kill or Die-03.png";
 import WhiteSkull from "./assets/Black - White Skull-03.png";
 import PubStreet from "./assets/Black - Pub Street-03.png";
+import GodofLight from "./assets/Black - God of Light-03.png";
 
 export default function ProductsSection() {
+
+    const [teeNum, setTeeNum] = useState(0);
+
     return (
         <nav className='Products-Section'>
             <div>
@@ -14,8 +19,20 @@ export default function ProductsSection() {
                 <ShirtComponent img={KillorDie} name={"Kill or Die"}/>
                 <ShirtComponent img={WhiteSkull} name={"White Skull"}/>
                 <ShirtComponent img={PubStreet} name={"Pub Street"}/>
+                <ShirtComponent img={GodofLight} name={"God of Light"}/>
+                <ShirtComponent img={PubStreet} name={"Pub Street"}/>
+                <ShirtComponent img={PubStreet} name={"Pub Street"}/>
+                <ShirtComponent img={PubStreet} name={"Pub Street"}/>
+                <ShirtComponent img={PubStreet} name={"Pub Street"}/>
+                <ShirtComponent img={PubStreet} name={"Pub Street"}/>
+                <ShirtComponent img={PubStreet} name={"Pub Street"}/>
+                <ShirtComponent img={PubStreet} name={"Pub Street"}/>
+                <ShirtComponent img={PubStreet} name={"Pub Street"}/>
+                <ShirtComponent img={PubStreet} name={"Pub Street"}/>
+                <ShirtComponent img={PubStreet} name={"Pub Street"}/>
+                <ShirtComponent img={PubStreet} name={"Pub Street"}/>
+                <ShirtComponent img={PubStreet} name={"Pub Street"}/>
             </div>
-            <button className="Products-Button" type="button" style={{cursor: "pointer"}}>Our Tee Zone is right here!</button>
         </nav>
     );
 }
--- src/ShirtComponent.jsx
+++ src/ShirtComponent.jsx
@@ -1,14 +1,26 @@
-
+import { useState } from 'react';
 
 export default function ShirtComponent({img, name}) {
 
+    const [teeNum, setTeeNum] = useState(1);
+    const [totalTeeNum, setTotalTeeNum] = useState(0);
+
+    function handleTeeNum() {
+        setTeeNum(teeNum + 1);
+        setTotalTeeNum(totalTeeNum + teeNum);
+        console.log(totalTeeNum);
+    }
+
     return (
+
+    
+
     <>
         <div className="Component-Shirt-Parent">
             <img className="Component-Shirt-Img" src={img} alt="Kill or Die" />
             <p className="Component-Shirt-Name">T-Shirt | {name}</p>
             <p className="Component-Shirt-Price">$8.99</p>
-            <button className="Component-Shirt-Button" type="button" style={{cursor: "pointer"}}>Add to cart</button>
+            <button className="Component-Shirt-Button" onClick={handleTeeNum} type="button" style={{cursor: "pointer"}}>Add to cart</button>
         </div>
     </>
     )
